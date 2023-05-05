04b698cf43aa16beab0274463f95b789dcce860f
Stephen Reynolds <mail@stephenreynolds.dev>
2023-05-05 11:07:29 -0400

    chore: remove some stuff from the base image

--- build.sh
+++ build.sh
@@ -33,21 +33,6 @@ for pkg in $(echo -e "$rpm_packages"); do \
 done
 echo "---"
 
-# install yafti to install flatpaks on first boot, https://github.com/ublue-os/yafti
-pip install --prefix=/usr yafti
-
-# add a package group for yafti using the packages defined in recipe.yml
-flatpaks=$(yq '.flatpaks[]' < /tmp/ublue-recipe.yml)
-# only try to create package group if some flatpaks are defined
-if [[ -n "$flatpaks" ]]; then
-    yq -i '.screens.applications.values.groups.Custom.description = "Flatpaks defined by the image maintainer"' /usr/etc/yafti.yml
-    yq -i '.screens.applications.values.groups.Custom.default = true' /usr/etc/yafti.yml
-    for pkg in $(echo -e "$flatpaks"); do \
-        yq -i ".screens.applications.values.groups.Custom.packages += [{\"$pkg\": \"$pkg\"}]" /usr/etc/yafti.yml
-    done
-fi
-echo "---"
-
 # run post-install scripts
 echo "-- Running post-install scripts defined in recipe.yml --"
 buildscripts=$(yq '.post-install[]' < /usr/etc/ublue-recipe.yml)
